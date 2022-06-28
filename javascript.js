23f0aa2a37adbe0241668a498804f085a9864997
MykolasMi <mykolas.miskinisss@gmail.com>
2022-06-28 23:06:15 +0300

    connect display and buttons via js and html, add parts of working calculator

--- javascript.js
+++ javascript.js
@@ -1,4 +1,112 @@
-//console.log (operate (0, 0, "/"))
+const display = document.getElementById('display');
+const btnAC = document.getElementById('clearAll');
+const btnDivide = document.getElementById('division');
+const btn7 = document.getElementById('seven');
+const btn8 = document.getElementById('eight');
+const btn9 = document.getElementById('nine');
+const btnMultiply = document.getElementById('multiplication');
+const btn4 = document.getElementById('four');
+const btn5 = document.getElementById('five');
+const btn6 = document.getElementById('six');
+const btnSubstract = document.getElementById('substraction');
+const btn1 = document.getElementById('one');
+const btn2 = document.getElementById('two');
+const btn3 = document.getElementById('three');
+const btnAdd = document.getElementById('addition');
+const btn0 = document.getElementById('zero');
+const btnDot = document.getElementById('dot');
+const btnC = document.getElementById('clear');
+const btnEqual = document.getElementById('equality');
+
+let equasion = "";
+let displayValue = "";
+
+
+btnAC.addEventListener('click', function(e) {
+    populateDisplay("");
+})
+btnDivide.addEventListener('click', function(e) {
+    assignOperator("/")
+    populateDisplay("/");
+})
+btn7.addEventListener('click', function(e) {
+    populateDisplay("7");
+})
+btn8.addEventListener('click', function(e) {
+    populateDisplay("8");
+})
+btn9.addEventListener('click', function(e) {
+    populateDisplay("9");
+})
+btnMultiply.addEventListener('click', function(e) {
+    assignOperator("*")
+    populateDisplay("*");
+})
+btn4.addEventListener('click', function(e) {
+    populateDisplay("4");
+})
+btn5.addEventListener('click', function(e) {
+    populateDisplay("5");
+})
+btn6.addEventListener('click', function(e) {
+    populateDisplay("6");
+})
+btnSubstract.addEventListener('click', function(e) {
+    assignOperator("-")
+    populateDisplay("-");
+})
+btn1.addEventListener('click', function(e) {
+    populateDisplay("1");
+})
+btn2.addEventListener('click', function(e) {
+    populateDisplay("2");
+})
+btn3.addEventListener('click', function(e) {
+    populateDisplay("3");
+})
+btnAdd.addEventListener('click', function(e) {
+    assignOperator("+")
+    populateDisplay("+");
+})
+btn0.addEventListener('click', function(e) {
+    populateDisplay("0");
+})
+btnDot.addEventListener('click', function(e) {
+    populateDisplay(".");
+})
+btnC.addEventListener('click', function(e) {
+    populateDisplay("error");
+})
+btnEqual.addEventListener('click', function(e) {
+    populateDisplay(operate(value1, value2, operator));
+})
+
+
+function assignOperator(operator) {
+
+}
+
+function populateDisplay(value) {
+
+
+    if (value == "") {
+        display.textContent = "";
+        displayValue="";
+    }
+    else if (value == "") { //pakeisti efektyviau
+
+    }
+    else {
+        display.textContent += value;
+        displayValue+=value;
+    }
+
+
+
+
+    console.log(displayValue);
+}
+
 
 function add(a, b) {
     return a + b;
